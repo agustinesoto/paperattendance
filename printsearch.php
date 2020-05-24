172e449728ab0b5e64a2641b09857abd7d5c9c1f
Juan Pablo Espinoza <juaespinoza@icloud.com>
2020-05-23 21:42:14 -0400

    fixing UI problems on large screens

--- printsearch.php
+++ printsearch.php
@@ -260,7 +260,7 @@ $pdfmodal = "
 				<h4 class='modal-title' id='pdfModalLabel'>Listas pdf</h4>
 			</div>
 			<div class='modal-body'>
-				<div class='pdflists' style='height: 80vh; display:flex; flex-direction: column; align-items: start;'></div>
+				<div class='pdflists' style='max-height: 80vh; display:flex; flex-direction: column; align-items: start;'></div>
 			</div>
 		</div>
 	</div>
